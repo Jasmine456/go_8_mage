cc1cb47c3f466e771738e919a3a3b1cf00732db8
jasmine <5265388+jasmine456@user.noreply.gitee.com>
2022-04-22 15:36:59 +0800

    补充了第一周的笔记，第三周的预习笔记刚开始写，第二周的笔记还没完成

--- week01/homework/main.go
+++ week01/homework/main.go
@@ -1,1 +1,34 @@
-package homework
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+//输出一个int32对应的二进制表示
+func BinaryFormat(n int32) string {
+	a := uint32(n)  //有符号转为无符号
+	sb := strings.Builder{}  //把多个短字符串拼接成一个长字符串
+	c := uint32(math.Pow(2, 31))  //最高位是1，其他位全是0
+	for i := 0; i < 32; i++ {
+		if a&c !=0{
+			sb.WriteString("1")   //往sb后面拼接“1”
+		} else {
+			sb.WriteString("0") //往sb后面拼接“0”
+		}
+		c >>= 1 //"1" 往右移一位
+	}
+	return sb.String()//返回长度为32的字符串
+}
+
+func main()  {
+	//c := uint32(math.Pow(2, 31))
+	//d := uint32(math.Pow(2, 2))
+	//fmt.Printf("c is %b, d is %b\n",c,d)
+	fmt.Println(BinaryFormat(0))
+	fmt.Println(BinaryFormat(100))
+	fmt.Println(BinaryFormat(-100))
+	fmt.Println(BinaryFormat(342553432))
+
+}
--- week02/array/main.go
+++ week02/array/main.go
@@ -1,1 +1,74 @@
-package array
+package main
+
+import "fmt"
+
+func basic(){
+	var arr1 [5]int =[5]int{}
+	var arr2 = [5]int{}
+	var arr3 = [5]int{1,2,4}
+	var arr4 = [5]int{2:8,3:8}
+	var arr5 =[...]int{1,2,4}
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+	fmt.Println(arr4)
+	fmt.Println(arr5)
+}
+
+func arrPoint(arr *[5]int){
+	fmt.Printf("%p\n",arr)
+	arr[0]+=10
+	fmt.Println(arr[0])
+
+
+
+}
+
+func array2d(){
+	var arr1 = [5][3]int{{1,2},{3},{6,3,8}}
+	//var arr2 = [...][3]int{{1,2},{6,3,8}}
+	//fmt.Println(len(arr1))
+	//fmt.Println(len(arr2))
+	//fmt.Println(cap(arr1))
+	//fmt.Println(cap(arr2))
+
+	//for i ,row := range arr1{
+	//	fmt.Printf("%T\n",row)
+	//	for j,n := range row{
+	//		fmt.Printf("%d %d %d\n",i,j,n)
+	//	}
+	//}
+
+	for i:=0;i<len(arr1);i++{
+		for j:=0;j<len(arr1[i]);j++{
+			fmt.Printf("%d %d %d\n",i,j,arr1[i][j])
+		}
+	}
+}
+
+
+func for_range(){
+	arr := [...]int{1,2,3,4,5}
+	for i ,ele := range arr{// ele是arr里元素的拷贝
+		fmt.Printf("%d %d\n",i,ele)
+		arr[i] += 8
+		fmt.Printf("%d %d\n",i,ele)
+	}
+}
+
+func main(){
+	//for_range()
+	//array2d()
+	//basic()
+	//var crr [5]int = [5]int{1,2,3,6,9}
+	//fmt.Printf("%p\n",&crr)
+	//fmt.Printf("%p\n",&crr[0])
+	//fmt.Printf("%p\n",&crr[1])
+	//arrPoint(&crr)
+	//fmt.Println(crr[0])
+	var a int = 9
+	var b *int
+	b = &a //取址
+	*b =3 // 解析指针
+
+}
